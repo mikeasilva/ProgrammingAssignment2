a4c5d10f8e6b90497eb9f97b6a15cd92dd61d3b1
Mike Silva <mike.a.silva@gmail.com>
2014-04-23 13:51:27 -0400

    Improved readability
    Added white space and clearer variable names

--- cachematrix.R
+++ cachematrix.R
@@ -1,66 +1,73 @@
-## Cache Matrix
-## 
-## Written by Mike Silva for the Coursera R Programming course
-##
-## These functions take a matrix and finds the inverse
-##
+# Cache Matrix
+# 
+# Written by Mike Silva for the Coursera R Programming course
+#
+# These functions take a matrix and finds the inverse
+#
 ################################################################################
-## EXAMPLE
-## > m <- matrix(c(1, 1, 1, 3, 4, 3, 3, 3, 4),3)
-## > m 
-## [,1] [,2] [,3]
-## [1,]    1    3    3
-## [2,]    1    4    3
-## [3,]    1    3    4
-##
-## >cache.matrix <- makeCacheMatrix(m) 
-## >m.inverse <- cacheSolve(cache.matrix)
-## >m.inverse
-## [,1] [,2] [,3]
-## [1,]    7   -3   -3
-## [2,]   -1    1    0
-## [3,]   -1    0    1
-## 
+# EXAMPLE
+# > m <- matrix(c(1, 1, 1, 3, 4, 3, 3, 3, 4),3)
+# > m 
+# [,1] [,2] [,3]
+# [1,]    1    3    3
+# [2,]    1    4    3
+# [3,]    1    3    4
+#
+# >cache.matrix <- makeCacheMatrix(m) 
+# >m.inverse <- cacheSolve(cache.matrix)
+# >m.inverse
+# [,1] [,2] [,3]
+# [1,]    7   -3   -3
+# [2,]   -1    1    0
+# [3,]   -1    0    1
+# 
 
-## This function creates a special "matrix" object that can cache its inverse.
+# This function creates a special "matrix" object that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-  ## This is the inverse matrix
-  m <- NULL
-  ## This function sets value of the matrix
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
+makeCacheMatrix <- function(input.matrix = matrix()) {
+  # Initialize the inverse matrix
+  inverse.matrix <- NULL
+  
+  # This function sets value of the matrix
+  set <- function(val) {
+    input.matrix <<- val
+    inverse.matrix <<- NULL
   }
-  ## This function gets the value of the matrix
-  get <- function() x
-  ## This function sets the value of the inverse matrix
-  setinverse <- function(cache.matrix) m <<- cache.matrix
-  ## This function gets the value of the inverse matrix
-  getinverse <- function() m
-  ## This is what the function returns
-  list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)  
+  
+  # This function gets the value of the matrix
+  get <- function() input.matrix
+  
+  # This function sets the value of the inverse matrix
+  setinverse <- function(cache.matrix) inverse.matrix <<- cache.matrix
+  
+  # This function gets the value of the inverse matrix
+  getinverse <- function() inverse.matrix
+  
+  # This is what the function returns
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-## This function computes the inverse of the special "matrix" returned by 
-## makeCacheMatrix above. If the inverse has already been calculated (and the 
-## matrix has not changed), then the cachesolve should retrieve the inverse from 
-## the cache.
+# This function computes the inverse of the special "matrix" returned by 
+# makeCacheMatrix above. If the inverse has already been calculated (and the 
+# matrix has not changed), then the cachesolve should retrieve the inverse from 
+# the cache.
 
-cacheSolve <- function(x, ...) {
-  ## Get the inverse from the cached function that is passed in
-  m <- x$getinverse()
-  ## Check if cached data exists
-  if(is.null(m)) {
-    ## Cached data does not exists so get the matrix
-    data <- x$get()
-    ## Find the inverse using solve
-    m <- solve(data, ...)
-    ## Cache the inverse
-    x$setinverse(m)
+cacheSolve <- function(makeCacheMatrix.list, ...) {
+  # Get the inverse from the cached function that is passed in
+  inverse.matrix <- makeCacheMatrix.list$getinverse()
+  
+  # Check if cached data exists
+  if(is.null(inverse.matrix)) {
+    # Cached data does not exists so get the matrix
+    data <- makeCacheMatrix.list$get()
+    
+    # Find the inverse using solve method
+    inverse.matrix <- solve(data, ...)
+    
+    # Cache the inverse
+    makeCacheMatrix.list$setinverse(inverse.matrix)
   }
-  ## Return a matrix that is the inverse of 'x'
-  m 
+  
+  # Return the special matrix that is the inverse of 'x'
+  inverse.matrix
 }
